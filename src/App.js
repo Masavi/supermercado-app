684a3a69576f424d97cc63c4f8fa69984f643935
maui <maui@devf.mx>
2020-04-10 21:02:50 -0500

    Agregar ejemplo de usos de JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 function App() {
+  const nombre = "Mauricio";
+  const apellido = "Saavedra";
+
+  const mostrarLorem = () => {
+    return (
+      <h2>lorem ipsum dolor si amet</h2>
+    )
+  }
+
+  const mostrarMensajae = () => {
+    return (<span> El nombre es: {nombre} </span>)
+  }
+
+  const mostrarCondicional = () => {
+    if (nombre === "Mauricio") {
+      return mostrarLorem()
+    } else return mostrarMensajae();
+  } 
+
   return (
-    <h1>Hola Mundo</h1>
+    <React.Fragment>
+      <h1>Hola Mundo</h1>
+      <p>{`${nombre} ${apellido}`}</p>
+      {
+        mostrarCondicional()
+      }
+    </React.Fragment>
   );
 }
 
