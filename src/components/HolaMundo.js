b250e2cac35386586ca8838e267b98c5d5b406ad
maui <maui@devf.mx>
2020-04-13 20:12:49 -0500

    Agregar ejemplo de uso de bootstrap y reactstrapp

--- src/components/HolaMundo.js
+++ src/components/HolaMundo.js
@@ -1,4 +1,9 @@
 import React, { Component } from 'react';
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
+// import './styles.css'; -> así importas tu propia hoja de estilos
 
 class HolaMundo extends Component {
   render() { 
@@ -6,6 +11,26 @@ class HolaMundo extends Component {
       <React.Fragment>
         <p>Hola desde el componente Hola Mundo</p>
         <p>Soy otro elemento dentro de un mismo nodo :D</p>
+        {/* <div class="card" style="width: 18rem;"> */}
+
+        {/* Codigo tomado directamente de la doc de Bootstrap */}
+        {/* <div className="card" style={{ width: "18rem" }}>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="google.com" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div> */}
+
+        {/* Código tomado de la doc de Reactstrapp */}
+        <Card>
+          <CardBody>
+            <CardTitle>Card title</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+            <Button>Button</Button>
+          </CardBody>
+        </Card>
       </React.Fragment>
     );
   }
